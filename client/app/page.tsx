2066e9b8b379edbe6644425233abab5993defe2f
zavvdev <zaviryukha.igor99@gmail.com>
2023-08-13 15:22:10 +0300

    main: fixed client hmr error; added api endpoints for client env variables

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,7 +1,22 @@
+import axios, { AxiosRequestConfig } from 'axios';
+
+const requestConfig: AxiosRequestConfig = {
+  baseURL: process.env.API_ENDPOINT || process.env.NEXT_PUBLIC_API_ENDPOINT,
+  headers: {
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+  },
+};
+
+const apiClient = axios.create(requestConfig);
+
 export default async function Home() {
+  const messageRequest = await apiClient.get('/v1/hello');
+
   return (
     <main>
       <h1>Crowd</h1>
+      Message: {messageRequest?.data ? JSON.stringify(messageRequest.data) : ''}
     </main>
   );
 }
--- client/next.config.js
+++ client/next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  serverRuntimeConfig: {
+    apiUrl: process.env.SERVER_API_ENDPOINT,
+  },
+  publicRuntimeConfig: {
+    apiUrl: process.env.CLIENT_API_ENDPOINT,
+  },
+};
 
 module.exports = nextConfig;
