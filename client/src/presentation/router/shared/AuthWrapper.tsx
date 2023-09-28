3c120a55f26918c3ca9790087a7a0621c1347d70
zavvdev <zaviryukha.igor99@gmail.com>
2023-09-28 15:42:14 +0300

    GRT-18: Removed cookieStorage

--- client/src/presentation/router/shared/AuthWrapper.tsx
+++ client/src/presentation/router/shared/AuthWrapper.tsx
@@ -1,12 +1,13 @@
 import { Navigate } from 'react-router-dom';
 import { PropsWithChildren } from 'react';
 import { PUBLIC_ROUTES } from '~/routes';
-import { isAuthenticated } from '~/application/features/auth/utilities';
 
 export function AuthWrapper({ children }: PropsWithChildren) {
+  const isAuthenticated = false;
+
   return (
     <>
-      {!isAuthenticated() ? (
+      {!isAuthenticated ? (
         <Navigate to={PUBLIC_ROUTES.auth.login()} />
       ) : (
         children
