959586607418157d670592252e589039052f8906
zavvdev <zaviryukha.igor99@gmail.com>
2023-10-03 21:43:20 +0300

    GRT-15: server - added profile entity

---
+++ server/database/migrations/2023_10_03_102855_create_profiles_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id')->unsigned()->nullable();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('image_url')->nullable();
+            $table->string('background_image_url')->nullable();
+            $table->text('bio')->nullable();
+            $table->date('date_of_birth')->nullable();
+            $table->string('country')->nullable();
+            $table->string('city')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('profiles');
+    }
+};
